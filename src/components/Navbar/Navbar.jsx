659c187f62fb579bdbe2a36c41b80dce9a69674c
Chris Wu <zezemint@gmail.com>
2022-06-07 19:39:08 -0700

    added hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,27 @@ import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 
 const Navbar = () => (
+
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="application logo" />
     </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href="#home">Home</a></li>
-      <li className='p__opensans'><a href="#about">About</a></li>
-      <li className='p__opensans'><a href="#menu">Menu</a></li>
-      <li className='p__opensans'><a href="#awards">Awards</a></li>
-      <li className='p__opensans'><a href="#contact">Contact</a></li>
+    <ul className="app__navbar-links">
+      <li className="p__opensans"><a href="#home">Home</a></li>
+      <li className="p__opensans"><a href="#about">About</a></li>
+      <li className="p__opensans"><a href="#menu">Menu</a></li>
+      <li className="p__opensans"><a href="#awards">Awards</a></li>
+      <li className="p__opensans"><a href="#contact">Contact</a></li>
     </ul>
-  </nav>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">Log In / Registration</a>
+      <div />
+      <a href="/" className="p__opensans">Reserve Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={25} onClick={() => {}} />
+    </div>
+  </nav >
 );
 
 export default Navbar;
