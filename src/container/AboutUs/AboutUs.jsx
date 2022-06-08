ad2ced40da77b4f0b7d2433e9842966ef94f59f5
Chris Wu <zezemint@gmail.com>
2022-06-08 12:35:12 -0700

    positioned Wu backdrop behind knife image

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -10,13 +10,22 @@ const AboutUs = () => (
     </div>
     <div className='app__aboutus-content flex__center'>
       <div className='app__aboutus-content_about'>
-        <h1 className='headtext cormorant'>About Us</h1>
+        <h1 className='headtext cormorant'>About us</h1>
         <img src={images.spoon} alt="about_spoon" className='spoon_img' />
         <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <button type='button' className="custom__button">Learn more</button>
       </div>
+
       <div className="app__aboutus-content_knife flex__center">
+        {/* from https://www.amazon.ca/dp/B09LRNL95J/ref=sbl_dpx_ca-kitchen-chef-knives_B088T58B2Z_0 */}
         <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext cormorant'>Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon_img' />
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button type='button' className="custom__button">Learn more</button>
       </div>
     </div>
   </div>
