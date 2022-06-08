b34a845d144487c56c65d8724d53f6b80c910e3e
Chris Wu <zezemint@gmail.com>
2022-06-08 11:35:59 -0700

    added the initial About Us section with lorem ipsum

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
 import './AboutUs.css';
+import { images } from '../../constants';
+
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src="images.Wu" alt="wu" />
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext cormorant'>About Us</h1>
+        <img src="images.spoon" alt="about_spoon" className='spoon_img' />
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button type='button' className="custom__button">Learn more</button>
+      </div>
+    </div>
   </div>
 );
 
