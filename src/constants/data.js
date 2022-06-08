1d61be0a9c3c7a75f99aa142b440f312935b7a30
Chris Wu <zezemint@gmail.com>
2022-06-08 15:24:14 -0700

    implemented data for cocktail and wine sections

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,57 +2,57 @@ import images from './images';
 
 const wines = [
   {
+    title: 'Asahi',
+    price: '$25',
+    tags: 'IE | 750 mL',
+  },
+  {
     title: 'Chapel Hill Shiraz',
-    price: '$56',
+    price: '$35',
     tags: 'AU | Bottle',
   },
   {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
+    title: 'Domaine de la Romanee Conti',
+    price: 'Req',
+    tags: 'FR | Bottle',
   },
   {
     title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
+    price: '$48 ',
+    tags: 'FR | 750 mL',
   },
   {
     title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
-  },
-  {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
+    price: '$50 ',
+    tags: 'CA | 750 mL',
   },
 ];
 
 const cocktails = [
   {
-    title: 'Aperol Sprtiz',
-    price: '$20',
-    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+    title: 'Bellini',
+    price: '$15',
+    tags: 'Prosecco | White Peach Purée ',
   },
   {
     title: "Dark 'N' Stormy",
-    price: '$16',
-    tags: 'Dark rum | Ginger beer | Slice of lime',
-  },
-  {
-    title: 'Daiquiri',
-    price: '$10',
-    tags: 'Rum | Citrus juice | Sugar',
+    price: '$17',
+    tags: 'Dark rum | Ginger Beer | Slice of Lime',
   },
   {
     title: 'Old Fashioned',
-    price: '$31',
-    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+    price: '$19',
+    tags: 'Bourbon | Brown Sugar | Angostura Bitters',
   },
   {
-    title: 'Negroni',
-    price: '$26',
-    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+    title: 'Passion Fruit Martini',
+    price: '$21',
+    tags: 'Vodka | Passoa | Lime Juice | Sugar',
+  },
+  {
+    title: 'Rum Punch',
+    price: '$23',
+    tags: 'Golden Rum | Citrus Juice | Pineapple and Cherry Garnish',
   },
 ];
 
