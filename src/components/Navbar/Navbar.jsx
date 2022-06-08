987ebb7c2c17d1a1f8bc1cee878155b629909fd8
Chris Wu <zezemint@gmail.com>
2022-06-07 19:11:41 -0700

    removed bullet points (list style) in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => (
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="application logo" />
     </div>
-    <ul className='app__navbar-logo'>
+    <ul className='app__navbar-links'>
       <li className='p__opensans'><a href="#home">Home</a></li>
       <li className='p__opensans'><a href="#about">About</a></li>
       <li className='p__opensans'><a href="#menu">Menu</a></li>
