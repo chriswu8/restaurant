e9310e0be8150a1811533e2cba1089e72a1dd69b
Chris Wu <zezemint@gmail.com>
2022-06-08 12:18:55 -0700

    inserted Wu backdrop and knife image

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => (
   <div>
     <Navbar />
     <Header />
-    {/* <AboutUs />
-    <SpecialMenu />
+    <AboutUs />
+    {/* <SpecialMenu />
     <Chef />
     <Intro />
     <Laurels />
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,14 +6,17 @@ import { images } from '../../constants';
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className='app__aboutus-overlay flex__center'>
-      <img src="images.Wu" alt="wu" />
+      <img src={images.Wu} alt="wu" />
     </div>
     <div className='app__aboutus-content flex__center'>
       <div className='app__aboutus-content_about'>
         <h1 className='headtext cormorant'>About Us</h1>
-        <img src="images.spoon" alt="about_spoon" className='spoon_img' />
+        <img src={images.spoon} alt="about_spoon" className='spoon_img' />
         <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <button type='button' className="custom__button">Learn more</button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
       </div>
     </div>
   </div>
