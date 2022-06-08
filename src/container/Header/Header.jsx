bb81804ade668042833650400014ea91901f0fd7
Chris Wu <zezemint@gmail.com>
2022-06-07 20:56:03 -0700

    implemented the header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,7 +5,18 @@ import { SubHeading } from '../../components';
 
 
 const Header = () => (
-  <div></div>
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Feel our dedication through our flavour" />
+      <h1 className="app__header-h1">The Finest Vancouver Dining Environment</h1>
+      <p className="p__opensans" id='lorem'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="welcome image" />
+    </div>
+  </div>
 );
 
 export default Header;
