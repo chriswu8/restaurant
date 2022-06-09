aa694e4320295d696ed45304a4bec87a19de0f94
Chris Wu <zezemint@gmail.com>
2022-06-09 00:06:24 -0700

    Reduced chef's words (lorem ipsum) and inserted chef's signature

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -15,8 +15,14 @@ const Chef = () => (
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote_image" />
-          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>
         </div>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Gordan Ramsay</p>
+        <p className="p__opensans">Head Chef</p>
+        <img src={images.sign} alt="sign_image" />
       </div>
 
 
