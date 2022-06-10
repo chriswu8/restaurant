a439c3f673a8695a1fde639c4b66599354d51fed
Chris Wu <zezemint@gmail.com>
2022-06-09 19:58:56 -0700

    repositioned awardDish in 450px media query

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import SubHeading from './SubHeading/SubHeading';
+import Heading from './Heading/Heading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './Menuitem/MenuItem';
@@ -6,6 +7,7 @@ import Navbar from './Navbar/Navbar';
 
 export {
   SubHeading,
+  Heading,
   Newsletter,
   FooterOverlay,
   MenuItem,
