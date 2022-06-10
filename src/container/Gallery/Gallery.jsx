3cfcd94cdb99860e53c174acebc320596af22b35
Chris Wu <zezemint@gmail.com>
2022-06-10 11:50:30 -0700

    Implemented initial gallery setup

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import './Gallery.css';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
-import './Gallery.css';
 
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <SubHeading title="instagram" />
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+    </div>
+
+  );
+}
+
 
 export default Gallery;
